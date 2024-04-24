d35af49a9ed4cb1f28aed3b388c42941c4533c6a
Lior Forer <liorfo@wix.com>
2024-04-25 00:39:34 +0300

    add classification training file + phi3 training

--- train_llm_classification.py
+++ train_llm_classification.py
@@ -3,13 +3,13 @@ import os
 import torch
 from datasets import load_dataset
 from transformers import (
-    AutoModelForCausalLM,
+    AutoModelForSequenceClassification,
     AutoTokenizer,
     BitsAndBytesConfig,
     TrainingArguments,
+    Trainer,
 )
 from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training, TaskType
-from trl import SFTTrainer
 from definition_handler.process_data import DatasetsHandler
 import pandas as pd
 import re
@@ -20,9 +20,9 @@ from datasets import Dataset, load_dataset
 ################################################################################
 # LoRA attention dimension
 # lora_r = 64
-lora_r = 32
+lora_r = 16
 # Alpha parameter for LoRA scaling
-lora_alpha = 64
+lora_alpha = 32
 # Dropout probability for LoRA layers
 lora_dropout = 0.05
 
@@ -42,7 +42,7 @@ use_nested_quant = False
 # TrainingArguments parameters
 ################################################################################
 # Output directory where the model predictions and checkpoints will be stored
-output_dir = '/cs/labs/tomhope/forer11/SciCo_Retrivel/phi3_4k_sft/no_def/model'
+output_dir = '/cs/labs/tomhope/forer11/SciCo_Retrivel/mistral_v2_sfttrainer/no_def/model'
 # Number of training epochs
 num_train_epochs = 1
 # Enable fp16/bf16 training (set bf16 to True with an A100)
@@ -60,13 +60,13 @@ gradient_checkpointing = True
 max_grad_norm = 0.3
 # Initial learning rate (AdamW optimizer)
 # learning_rate = 2e-4
-learning_rate = 2e-4
+learning_rate = 0.00001
 # Weight decay to apply to all layers except bias/LayerNorm weights
 weight_decay = 0.001
 # Optimizer to use
-optim = 'paged_adamw_32bit'
+optim = "paged_adamw_8bit"
 # Learning rate schedule (constant a bit better than cosine)
-lr_scheduler_type = "cosine"
+lr_scheduler_type = "constant"
 # Number of training steps (overrides num_train_epochs)
 max_steps = -1
 # Ratio of steps for a linear warmup (from 0 to learning rate)
@@ -75,7 +75,7 @@ warmup_ratio = 0.03
 # Saves memory and speeds up training considerably
 group_by_length = True
 # Save checkpoint every X updates steps
-save_steps = 200
+save_steps = 10
 # Log every X updates steps
 logging_steps = 10
 
@@ -85,7 +85,7 @@ logging_steps = 10
 # Maximum sequence length to use
 max_seq_length = 1024  # None
 # Pack multiple short examples in the same input sequence to increase efficiency
-packing = True  # False
+packing = False  # False
 # Load the entire model on the GPU 0
 # device_map = {"": 0}
 device_map = "auto"
@@ -112,31 +112,6 @@ second term context: {term2_text}
 
 out_prompt = """### Output:
 {label}"""
-
-
-phi3_instruct_prompt = """<|system|>
-You are a helpful AI assistant. you will get two scientific texts that has a term surrounded by a relevant context. Read the terms with their context and define the correct relationship between the two terms as follows:
-1 - Term A and term B are co-referring terms
-2 - Term A is a parent concept of term B
-3 - Term A is a child concept of term B
-0 - None of the above relations are appropriate<|end|>
-<|user|>
-define the relationship between the two terms with the following data:
-first term: {term1} 
-first term context: {term1_text}
-
-second term: {term2}
-second term context: {term2_text}<|end|>
-<|assistant|>
-{label}"""
-
-def get_phi3_instruct_prompt(pair, label):
-    term1_text, term2_text, _ = pair.split('</s>')
-    term1 = re.search(r'<m>(.*?)</m>', term1_text).group(1).strip()
-    term2 = re.search(r'<m>(.*?)</m>', term2_text).group(1).strip()
-    term1_text, term2_text = term1_text.replace('<m> ', '').replace(' </m>', ''), term2_text.replace('<m> ','').replace(' </m>', '')
-    return phi3_instruct_prompt.format(term1=term1, term1_text=term1_text, term2=term2, term2_text=term2_text, label=label)
-
 
 
 def get_task_prompt(with_def=False):
@@ -175,11 +150,6 @@ def format_prompts_fn(example):
 
 base_model = "microsoft/Phi-3-mini-4k-instruct"
 
-# base_model = "alpindale/Mistral-7B-v0.2-hf"
-
-# base_model = "mistralai/Mistral-7B-v0.1"
-
-
 data = DatasetsHandler(test=False, train=True, dev=True)
 
 bnb_config = BitsAndBytesConfig(
@@ -189,24 +159,19 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_use_double_quant=use_nested_quant,  # False
 )
 
-model = AutoModelForCausalLM.from_pretrained(
+model = AutoModelForSequenceClassification.from_pretrained(
     base_model,
     quantization_config=bnb_config,
     cache_dir='/cs/labs/tomhope/forer11/cache',
-    trust_remote_code=True,
-    attn_implementation="flash_attention_2",
-    device_map=device_map
+    device_map=device_map,
+    trust_remote_code=True
 )
 
+print(model)
+
 tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
-
-tokenizer.pad_token = tokenizer.unk_token  # use unk rather than eos token to prevent endless generation
-tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
-tokenizer.padding_side = 'right'
-
-
-# tokenizer.pad_token = tokenizer.eos_token
-# tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
 
 # Load LoRA configuration
 peft_config = LoraConfig(
@@ -215,7 +180,7 @@ peft_config = LoraConfig(
     r=lora_r,
     bias="none",
     task_type=TaskType.CAUSAL_LM,
-    target_modules=["o_proj", "qkv_proj", "gate_up_proj", "down_proj"]
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", 'down_proj']
 )
 
 # Set training parameters
@@ -239,11 +204,8 @@ training_arguments = TrainingArguments(
     report_to="wandb",
 )
 
-# train_prompts = [{'text': get_format_prompt(data.train_dataset.pairs[i], data.train_dataset.natural_labels[i])} for i in
-#                  range(len(data.train_dataset))]
-train_prompts = [{'text': get_phi3_instruct_prompt(data.train_dataset.pairs[i], data.train_dataset.natural_labels[i])} for i in
+train_prompts = [{'text': get_format_prompt(data.train_dataset.pairs[i], data.train_dataset.natural_labels[i])} for i in
                  range(len(data.train_dataset))]
-
 # train_data = pd.DataFrame(train_prompts, columns=['text'])
 # dataset = Dataset.from_pandas(train_data)
 
@@ -253,7 +215,7 @@ dataset = Dataset.from_list(train_prompts)
 # dataset111 = load_dataset(dataset_name, split="train[0:1000]")
 
 # Set supervised fine-tuning parameters
-trainer = SFTTrainer(
+trainer = Trainer(
     model=model,
     train_dataset=dataset,
     peft_config=peft_config,
